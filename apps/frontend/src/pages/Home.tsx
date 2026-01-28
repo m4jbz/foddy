e9f9816a7d67e5bd4220397dc81b01571a254382
SirEduardOo <najeraeduardo414@gmail.com>
2026-01-27 21:00:38 -0600

    chore: setup React + Vite fronted base con Tailwind y dependencias principales

---
+++ apps/frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1 className="min-h-screen flex items-center justify-center bg-gray-100 text-4xl font-bold text-orange-600">Foddy Frontend - Home</h1>
+}
+
+export default Home
